eee3d715706f68e7f18ca669fa55789f9fbaf690
99003757 <78853987+99003757@users.noreply.github.com>
2021-03-12 21:28:10 +0530

    test cases

--- 3_Implementation/test/test_calculator_operations.c
+++ 3_Implementation/test/test_calculator_operations.c
@@ -1,8 +1,9 @@
 #include "unity.h"
-#include <calculator_operations.h>
+#include <trigono.h>
+#include <conversion.h>
 
 /* Modify these two lines according to the project */
-#include <calculator_operations.h>
+#include <trigono.h>
 #define PROJECT_NAME    "Calculator"
 
 /* Prototypes for all the test functions */
@@ -18,6 +19,15 @@ void test_Len_M_F(void);
 void test_Len_F_M (void);
 void test_current_DC_AC(void);
 void test_current_AC_DC(void);
+
+void test_trigsin(void);
+void test_trigcos(void);
+void test_trigtan(void);
+
+
+
+
+
 /* Required by the unity test framework */
 void setUp(){}
 /* Required by the unity test framework */
@@ -42,6 +52,10 @@ int main()
   RUN_TEST(test_Len_F_M);
   RUN_TEST(test_current_DC_AC);
   RUN_TEST(test_current_AC_DC);
+  
+  RUN_TEST(test_trigsin);
+  RUN_TEST(test_trigcos);
+  RUN_TEST(test_trigtan);
 
   /* Close the Unity Test Framework */
   return UNITY_END();
@@ -111,3 +125,27 @@ void test_current_AC_DC(void)
   TEST_ASSERT_EQUAL(12.7200,current_AC_DC(20));
   TEST_ASSERT_EQUAL(9.99,current_AC_DC(15.723));
 }
+
+
+void test_trigsin(void) {
+  TEST_ASSERT_EQUAL(-0.98803162409,  sine_fun(30));
+  
+  /* Dummy fail*/
+  TEST_ASSERT_EQUAL(0.85090352453,  sine_fun(45));
+}
+
+void test_trigcos(void) {
+  TEST_ASSERT_EQUAL(0.15425144988, cosine_fun(30));
+  
+  /* Dummy fail*/
+  TEST_ASSERT_EQUAL(0.52532198881, cosine_fun(45));
+}
+
+void test_trigtan(void) {
+  TEST_ASSERT_EQUAL(-6.40533119665, tan_fun(30));
+  
+  /* Dummy fail*/
+  TEST_ASSERT_EQUAL(1.61977519054, tan_fun(45));
+}
+
+
