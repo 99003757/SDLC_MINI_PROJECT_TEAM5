e3c70d14bfdc26cc3641064121dce44a62547c3d
99003752 <78851798+99003752@users.noreply.github.com>
2021-03-13 19:32:10 +0530

    Update Base_Conversion.h

--- 3_Implementation/inc/Base_Conversion.h
+++ 3_Implementation/inc/Base_Conversion.h
@@ -2,7 +2,7 @@
 #define __BASE_CONVERSION_H__
 #include <stdio.h>
 #include<stdlib.h>
-
+//Function Declaration
 int convert_dec_to_bin(int dec_no);
 int convert_bin_to_dec(int bin_no);
 int convert_dec_to_oct(int decimalNumber);
