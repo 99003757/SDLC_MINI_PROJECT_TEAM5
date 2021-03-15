352f3640572f2577db21b87e6ee29b452aa87a65
99003761 <78851073+99003761@users.noreply.github.com>
2021-03-15 09:25:24 +0530

    Update test_calculator_operations.c

--- 3_Implementation/test/test_calculator_operations.c
+++ 3_Implementation/test/test_calculator_operations.c
@@ -27,6 +27,15 @@ void test_Len_F_M (void);
 void test_current_DC_AC(void);
 void test_current_AC_DC(void);
 
+void test_temp_C_F2(void);
+void test_temp_F_C2(void);
+void test_Len_M_F2(void);
+void test_Len_F_M2 (void);
+void test_current_DC_AC2(void);
+void test_current_AC_DC2(void);
+
+void test_temp_F_C3(void);
+
 //99003757
 void test_trigsin(void);
 void test_trigcos(void);
@@ -80,11 +89,7 @@ int main()
   UNITY_BEGIN();
 
 /* Run Test functions */
-  //RUN_TEST(test_add);
-  //RUN_TEST(test_add_testcase2);
-  //RUN_TEST(test_subtract);
-  //RUN_TEST(test_multiply);
-  //RUN_TEST(test_divide);
+ 
   /////////99003761//////
   RUN_TEST(test_temp_C_F);
   RUN_TEST(test_temp_F_C);
@@ -92,6 +97,16 @@ int main()
   RUN_TEST(test_Len_F_M);
   RUN_TEST(test_current_DC_AC);
   RUN_TEST(test_current_AC_DC);
+  
+  RUN_TEST(test_temp_C_F2);
+  RUN_TEST(test_temp_F_C2);
+  RUN_TEST(test_Len_M_F2);
+  RUN_TEST(test_Len_F_M2);
+  RUN_TEST(test_current_DC_AC2);
+  RUN_TEST(test_current_AC_DC2);
+  
+  RUN_TEST(test_temp_F_C3);
+  
   
   RUN_TEST(test_trigsin);
   RUN_TEST(test_trigcos);
@@ -108,9 +123,9 @@ int main()
   RUN_TEST(test_trigcot1);
 
   RUN_TEST(test_convert_dec_to_bin1);
-RUN_TEST(test_convert_bin_to_dec1);
-RUN_TEST(test_convert_dec_to_oct1);
-RUN_TEST(test_convert_oct_to_dec1);
+  RUN_TEST(test_convert_bin_to_dec1);
+  RUN_TEST(test_convert_dec_to_oct1);
+  RUN_TEST(test_convert_oct_to_dec1);
   
 //Run test for Base conversions
   RUN_TEST(test_convert_dec_to_bin);
@@ -128,71 +143,71 @@ RUN_TEST(test_convert_oct_to_dec1);
   return UNITY_END();
 }
 
-/* 
-Write all the test functions 
-void test_add(void) {
-  TEST_ASSERT_EQUAL(30, add(10, 20));
-  TEST_ASSERT_EQUAL(-10, add(10, -20));
-}
-void test_add_testcase2(void) {
- 
-  /* Dummy fail
-  TEST_ASSERT_EQUAL(1500, add(750, 7500));
-}
-void test_subtract(void) {
-  TEST_ASSERT_EQUAL(-3, subtract(0, 3));
-  
-  /* Dummy fail
-  TEST_ASSERT_EQUAL(1, subtract(1000, 900));
-}
 
-void test_multiply(void) {
-  TEST_ASSERT_EQUAL(0, multiply(1, 0));
-  
-  /* Dummy fail
-  TEST_ASSERT_EQUAL(2, multiply(2, 5));
-}
-
-void test_divide(void) {
-  TEST_ASSERT_EQUAL(0, divide(1, 0));
-  
-  /* Dummy fail
-  TEST_ASSERT_EQUAL(3, divide(2, 2));
-}
-*/
 ///////////////////////////////99003761 Test_Case//////////////////////////////////////////////////////////
 void test_temp_C_F(void)
 {
   TEST_ASSERT_EQUAL(96.6200, temp_C_F(35.9));
   TEST_ASSERT_EQUAL(140.00, temp_C_F(60));
 }
+void test_temp_C_F2(void)
+{
+  TEST_ASSERT_EQUAL(140.00, temp_C_F(60));
+}
 
 void test_temp_F_C(void)
 {
   TEST_ASSERT_EQUAL(62.7777, temp_F_C(145));
+}
+void test_temp_F_C2(void)
+{
   TEST_ASSERT_EQUAL(36.444443, temp_F_C(97.6));
-  TEST_ASSERT_EQUAL(35.9, temp_F_C(96.62));
+
+}
+
+void test_temp_F_C3(void)
+{
+ TEST_ASSERT_EQUAL(35.9, temp_F_C(96.62));
 }
 
 void test_Len_M_F(void)
 {
   TEST_ASSERT_EQUAL(8.2021, Len_M_F(2.5));
+  
+}
+void test_Len_M_F2(void)
+{
+  
   TEST_ASSERT_EQUAL(9.8425, Len_M_F(3));
 }
+
 void test_Len_F_M (void)
 {
   TEST_ASSERT_EQUAL(3.048,Len_F_M(10));
+  
+}
+void test_Len_F_M2 (void)
+{
   TEST_ASSERT_EQUAL(2.49,Len_F_M(8.2));
 }
+
 void test_current_DC_AC(void)
 {
   TEST_ASSERT_EQUAL(15.723,current_DC_AC(10));
+  
+}
+void test_current_DC_AC2(void)
+{
   TEST_ASSERT_EQUAL(19.654,current_DC_AC(12.5));
 }
 void test_current_AC_DC(void)
 {
   TEST_ASSERT_EQUAL(12.7200,current_AC_DC(20));
-  TEST_ASSERT_EQUAL(9.99,current_AC_DC(15.723));
+  
+}
+void test_current_AC_DC2(void)
+{
+ TEST_ASSERT_EQUAL(9.99,current_AC_DC(15.723));
 }
 
 ///////////////////////////////99003757 Test_Case//////////////////////////////////////////////////////////
